c6069f4529ea22035c0ac468ee05ede6e993dc72
Zoe <zoeerispe@gmail.com>
2021-08-27 16:27:13 +0200

    updated

--- javaScript/home.js
+++ javaScript/home.js
@@ -19,9 +19,13 @@ function showPosts() {
             ).innerHTML += `<div class="post">
                                 <div class="post-image"><img class="image" src="${post[1]}" alt="${post[2]}" /></div>
                                 <h2 class="title">${post[2]}</h2>
-                                <h3 class="dateCreated">${post[5]}</h3>
-                                <h3 class="content">${post[3]}</h3>
-                                <h3 class="author">${post[4]}</h3>
+                                <h3 class="dateCreated">${post[7]}</h3>
+                                <div class="content">
+                                    <h3 class="intro">${post[3]}</h3>
+                                    <h3 class="body">${post[4]}</h3>
+                                    <h3 class="conclusion">${post[5]}</h3>
+                                </div>
+                                <h3 class="author">${post[6]}</h3>
                             </div>`
             postNo += 1
         });
@@ -29,3 +33,7 @@ function showPosts() {
 }
 
 showPosts();
+
+function likePost() {
+    fetch("https://shrouded-temple-45259.herokuapp.com/like-post/")
+}
